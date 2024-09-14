b2104d122f13b6b6192bc0846c67f1787c0dcfb2
Azwa Arshad <azwaarshad1194@gmail.com>
2024-09-14 14:09:26 +0500

    Add files via upload

---
+++ Payment.java
@@ -0,0 +1,6 @@
+public class Payment {
+    public boolean processPayment(double amount) {
+        System.out.println("Processing payment of $" + amount);
+        return true; 
+    }
+}
---
+++ RoomCategory.java
@@ -0,0 +1,3 @@
+public enum RoomCategory {
+    SINGLE, DOUBLE, SUITE
+}
